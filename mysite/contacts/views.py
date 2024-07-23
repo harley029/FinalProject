6215369d68e2f94c6a5658c3f50b8b403bc08a5c
Oleksandr Kharchenko <harley029@gmail.com>
2024-07-23 14:01:03 +0200

    Minor adjasment

--- mysite/contacts/views.py
+++ mysite/contacts/views.py
@@ -10,7 +10,8 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse, reverse_lazy, timezone
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
 from django.shortcuts import get_object_or_404
 from datetime import timedelta
 
